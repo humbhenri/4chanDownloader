d20fe6abca17c41bcaedb9d86888838aa521f53c
humberto <humbhenri@gmail.com>
2015-10-17 14:03:25 -0300

    use 4chan api

--- 4chan.go
+++ 4chan.go
@@ -3,20 +3,107 @@ package main
 import (
 	"errors"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
-	"strconv"
 
-	"time"
-
-	"net/url"
+	"io/ioutil"
 	"strings"
 
-	"golang.org/x/net/html"
-
 	"github.com/cheggaaa/pb"
+
+	"encoding/json"
 )
+
+type JSON4ChanPost struct {
+	Tim      int
+	Filename string
+	Ext      string
+	Board    string
+}
+
+type JSON4ChanThread struct {
+	Title string
+	Posts []*JSON4ChanPost
+}
+
+func GetJson(url string) (*JSON4ChanThread, error) {
+	urlArr := strings.Split(url, "/")
+	jsonlink := fmt.Sprintf("http://a.4cdn.org/%s/thread/%s.json",
+		urlArr[3], urlArr[5])
+	res, err := http.Get(jsonlink)
+	if err != nil {
+		return nil, err
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var data JSON4ChanThread
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+	for _, p := range data.Posts {
+		p.Board = urlArr[3]
+	}
+	data.Title = urlArr[len(urlArr)-1]
+	return &data, nil
+}
+
+func DownloadPostImage(post JSON4ChanPost) error {
+	link := fmt.Sprintf("http://images.4chan.org/%s/src/%d%s",
+		post.Board, post.Tim, post.Ext)
+	res, err := http.Get(link)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	file, err := os.Create(post.Filename + post.Ext)
+	if err != nil {
+		return err
+	}
+	contents, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(file.Name(), contents, 0644)
+	if err != nil {
+		return err
+	}
+	return file.Close()
+}
+
+func DownloadThread(url string) error {
+	thread, err := GetJson(url)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(thread.Title); os.IsNotExist(err) {
+		err = os.Mkdir(thread.Title, 0777)
+		if err != nil {
+			return err
+		}
+	}
+	os.Chdir(thread.Title)
+	count := 0
+	for _, post := range thread.Posts {
+		if post.Tim != 0 {
+			count = count + 1
+		}
+	}
+	bar := pb.StartNew(count)
+	for _, post := range thread.Posts {
+		if post.Tim != 0 {
+			err = DownloadPostImage(*post)
+			if err != nil {
+				return err
+			}
+			bar.Increment()
+		}
+	}
+	bar.Finish()
+	return nil
+}
 
 // Download4Chan Download all the images of a 4chan thread
 // cmdArgs - command line arguments
@@ -26,144 +113,7 @@ func Download4Chan(cmdArgs []string) error {
 	}
 
 	threadURL := cmdArgs[1]
-
-	threadTitle := getThreadTitle(threadURL)
-	if threadTitle == "" {
-		return errors.New("cannot get title of thread")
-	}
-
-	dir, err := createThreadImagesDir(threadTitle)
-	if err != nil {
-		return err
-	}
-
-	images, err := getImagesURL(threadURL)
-	if err != nil {
-		return err
-	}
-
-	errChan := make(chan error)
-	successChan := make(chan bool)
-	remainingItems := len(images)
-	progressBar := pb.StartNew(remainingItems)
-
-	for _, img := range images {
-		go downloadImage(dir, img, successChan, errChan)
-	}
-
-	for {
-		select {
-		case _ = <-successChan:
-			remainingItems--
-			progressBar.Increment()
-		case err := <-errChan:
-			return err
-		}
-
-		if remainingItems == 0 {
-			progressBar.FinishPrint(dir + " saved")
-			break
-		}
-	}
-
-	return nil
-}
-
-func downloadImage(dir string, img string, successChan chan bool, errChan chan error) {
-	imgURL, err := url.Parse(img)
-	if err != nil {
-		errChan <- err
-		return
-	}
-	// create file
-	frags := strings.Split(imgURL.Path, "/")
-	imgName := dir + string(os.PathSeparator) + frags[len(frags)-1]
-	file, err := os.Create(imgName)
-	if err != nil {
-		errChan <- err
-		return
-	}
-	defer file.Close()
-
-	// download image data
-	check := http.Client{
-		CheckRedirect: func(r *http.Request, via []*http.Request) error {
-			r.URL.Opaque = r.URL.Path
-			return nil
-		},
-	}
-	resp, err := check.Get("http:" + img) // add a filter to check redirect
-	if err != nil {
-		errChan <- err
-		return
-	}
-	defer resp.Body.Close()
-
-	_, err = io.Copy(file, resp.Body)
-	if err != nil {
-		errChan <- err
-		return
-	}
-
-	successChan <- true
-}
-
-func createThreadImagesDir(title string) (dir string, err error) {
-	dir = title + strconv.FormatInt(time.Now().UnixNano(), 10)
-	err = os.Mkdir(dir, 0777)
-	return
-}
-
-func getThreadTitle(thread string) (title string) {
-	threadURL, err := url.Parse(thread)
-	if err != nil {
-		return thread
-	}
-	frags := strings.Split(threadURL.Path, "/")
-	return frags[len(frags)-1]
-}
-
-func getImagesURL(thread string) (images []string, err error) {
-	// get thread url
-	resp, err := http.Get(thread)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-
-	// parse html
-	doc, err := html.Parse(resp.Body)
-	if err != nil {
-		return
-	}
-
-	nodeIsFileThumb := func(n *html.Node) (isFileThumb bool, href string) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			isFileThumb = false
-			for _, a := range n.Attr {
-				if a.Key == "class" && a.Val == "fileThumb" {
-					isFileThumb = true
-				}
-				if a.Key == "href" {
-					href = a.Val
-				}
-			}
-		}
-		return
-	}
-
-	var parse func(*html.Node)
-	parse = func(n *html.Node) {
-		fileThumb, href := nodeIsFileThumb(n)
-		if fileThumb {
-			images = append(images, href)
-		}
-		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			parse(c)
-		}
-	}
-	parse(doc)
-	return
+	return DownloadThread(threadURL)
 }
 
 func main() {
--- 4chan_test.go
+++ 4chan_test.go
@@ -1,40 +1,59 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"path"
+	"testing"
+)
 
-func TestThreadUrlShouldBePresent(t *testing.T) {
-	osArgs := []string{}
-	err := Download4Chan(osArgs)
-	noURLError := "must inform thread url"
-	if err.Error() != noURLError {
-		t.Error("Must inform " + noURLError)
+func TestGetJson(t *testing.T) {
+	url := "http://boards.4chan.org/g/thread/50843193/what-is-the-biggest-tech-failure-of-the-last-5"
+	data, err := GetJson(url)
+	if err != nil {
+		t.Errorf("get json shoud not return an error in this context: %s\n",
+			err.Error())
+	}
+	if len(data.Posts) == 0 {
+		t.Errorf("should be more than one post but was none")
+	}
+	expectedTitle := "what-is-the-biggest-tech-failure-of-the-last-5"
+	if data.Title != expectedTitle {
+		t.Errorf("title should be %s but was %s\n", expectedTitle, data.Title)
+	}
+	for _, post := range data.Posts {
+		if post.Board != "g" {
+			t.Errorf("board should be %s but was %s\n", "g", post.Board)
+		}
 	}
 }
 
-func TestGetTitleFromEmptyUrl(t *testing.T) {
-	title := getThreadTitle("")
-	if title != "" {
-		t.Error("empty url")
+func TestDownloadImage(t *testing.T) {
+	post := JSON4ChanPost{
+		Tim:      1445029221779,
+		Filename: "serveimage",
+		Ext:      ".jpg",
+		Board:    "g",
+	}
+	os.Chdir(os.TempDir())
+	err := DownloadPostImage(post)
+	if err != nil {
+		t.Errorf("error: %s\n", err.Error())
+	}
+	filename := "serveimage.jpg"
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		t.Errorf("no such file %s\n", filename)
 	}
 }
 
-func TestGetTitleFromThreadUrl(t *testing.T) {
-	title := getThreadTitle("abcd")
-	if title != "abcd" {
-		t.Errorf("expected %s but was %s", "abcd", title)
+func TestDownloadThread(t *testing.T) {
+	url := "http://boards.4chan.org/g/thread/50843193/what-is-the-biggest-tech-failure-of-the-last-5"
+	os.Chdir(os.TempDir())
+	err := DownloadThread(url)
+	if err != nil {
+		t.Errorf("error: %s\n", err.Error())
 	}
-}
-
-func TestGetTitleFromThreadUrl2(t *testing.T) {
-	title := getThreadTitle("xyz/abcd")
-	if title != "abcd" {
-		t.Errorf("expected %s but was %s", "abcd", title)
-	}
-}
-
-func TestGetTitleFromThreadUrl3(t *testing.T) {
-	title := getThreadTitle("foo/xyz/abcd")
-	if title != "abcd" {
-		t.Errorf("expected %s but was %s", "abcd", title)
+	dir := path.Join(os.TempDir(), "what-is-the-biggest-tech-failure-of-the-last-5")
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		t.Errorf("no such file %s\n", dir)
 	}
 }
